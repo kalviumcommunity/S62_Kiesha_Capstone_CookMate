7052594be72fe130b83368dfb41a3c4e08dabae5
Kiesha Maria <kiesha.maria@kalvium.community>
2025-04-26 21:41:01 +0530

    completed authentication using jwt

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const authRoutes= require('./routes/authRoutes');
+const userRoutes= require('./routes/userRoutes');
+
+
 
 dotenv.config();
 
@@ -10,6 +14,9 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use('/auth', authRoutes);
+app.use('/user',userRoutes);
+
 
 
 
