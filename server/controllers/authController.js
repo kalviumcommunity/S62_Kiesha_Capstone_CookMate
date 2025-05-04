8ff5fb64994d66534f2c4d57a2fdb2a166645f14
Kiesha Maria <kiesha.maria@kalvium.community>
2025-05-04 14:52:11 +0530

    updated jwt-authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -39,7 +39,8 @@ const login = async (req, res) => {
     if (!match) return res.status(400).json({ message: 'Invalid credentials' });
 
     const token = createToken(user._id);
-    res.status(200).json({ mesage:"User logged in successfully",token, user });
+    user.password = undefined;
+    res.status(200).json({ message:"User logged in successfully",token, user });
   } catch (err) {
     res.status(500).json({ message: 'Server error', error: err.message });
   }
